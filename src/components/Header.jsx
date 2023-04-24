ca4f1c0ebffe244b4ff7dc6d8d86db521c8d08e6
Jeffrey Stinson <jeffstinson@hotmail.com>
2023-04-24 14:05:41 -0700

    create Header component and html

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <header>
+      <h1>Jeff Stinson</h1>
+      <div>
+        <h2>About Me</h2>
+        <h2>Projects</h2>
+        <h2>Contact</h2>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
