a46c74d8d3a5e8b8fca6f9e9783032e7956499e3
Jeffrey Stinson <jeffstinson@hotmail.com>
2023-04-24 15:38:53 -0700

    add link underline animation

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,15 +1,5 @@
-
 const Body = () => {
-  return (
-    <header>
-      <h1>Jeff Stinson</h1>
-      <div>
-        <h2>About Me</h2>
-        <h2>Projects</h2>
-        <h2>Contact</h2>
-      </div>
-    </header>
-  );
+  return <div>About me</div>;
 };
 
 export default Body;
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,10 +4,12 @@ const Header = () => {
   return (
     <header className="header">
       <h1>Jeff Stinson</h1>
-      <div>
-        <h2>About Me</h2>
-        <h2>Projects</h2>
-        <h2>Contact</h2>
+      <div className="header-links">
+        <a href="About">About Me</a>
+        <a href="Projects">Projects</a>
+        <a href="Skills">Skills</a>
+        <a href="Resume">Resume</a>
+        <a href="Contact">Contact</a>
       </div>
     </header>
   );
