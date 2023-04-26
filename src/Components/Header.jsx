91fcadb49c1f0058fb805629d9c5b1344b3b8221
Smoopfrog <jeffstinson@hotmail.com>
2023-04-26 01:36:33 -0700

    add change display of header on scroll function

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,5 +1,5 @@
 import "../Styles/Header.css";
-
+import { useEffect, useState } from "react";
 const Header = () => {
   const handleClickScroll = (id) => {
     const element = document.getElementById(id);
@@ -8,15 +8,45 @@ const Header = () => {
       element.scrollIntoView({ behavior: "smooth" });
     }
   };
+  function useScrollDirection() {
+    const [scrollDirection, setScrollDirection] = useState(null);
+
+    useEffect(() => {
+      let lastScrollY = window.pageYOffset;
+
+      const updateScrollDirection = () => {
+        const scrollY = window.pageYOffset;
+        const direction = scrollY > lastScrollY ? "down" : "up";
+        if (
+          direction !== scrollDirection &&
+          (scrollY - lastScrollY > 10 || scrollY - lastScrollY < -10)
+        ) {
+          setScrollDirection(direction);
+        }
+        lastScrollY = scrollY > 0 ? scrollY : 0;
+      };
+      window.addEventListener("scroll", updateScrollDirection); // add event listener
+      return () => {
+        window.removeEventListener("scroll", updateScrollDirection); // clean up
+      };
+    }, [scrollDirection]);
+    console.log(scrollDirection);
+    return scrollDirection;
+  }
+
+  const scrollDirection = useScrollDirection();
 
   return (
-    <header className="header">
+    // <header className="header">
+    <header
+      className={`header sticky ${
+        scrollDirection === "down" ? "-top-24 down" : "top-0"
+      } h-24 bg-blue-200`}
+    >
       <h1>Jeff Stinson</h1>
       <nav className="header-links">
-        <button onClick={() => handleClickScroll("about")} >About Me</button>
-        <button onClick={() => handleClickScroll("projects")}>
-          Projects
-        </button>
+        <button onClick={() => handleClickScroll("about")}>About Me</button>
+        <button onClick={() => handleClickScroll("projects")}>Projects</button>
         <button onClick={() => handleClickScroll("skills")}>Skills</button>
         <button onClick={() => handleClickScroll("resume")}>Resume</button>
         <button onClick={() => handleClickScroll("contact")}>Contact</button>
