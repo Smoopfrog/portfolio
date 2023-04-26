db521815935af3ce122ad5239cb7d0e9b9a5abb1
Jeffrey Stinson <jeffstinson@hotmail.com>
2023-04-25 23:46:08 -0700

    change header links to buttons

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,8 +3,8 @@ import "../Styles/Header.css";
 const Header = () => {
   const handleClickScroll = (id) => {
     const element = document.getElementById(id);
+
     if (element) {
-      // 👇 Will scroll smoothly to the top of the next section
       element.scrollIntoView({ behavior: "smooth" });
     }
   };
@@ -12,15 +12,15 @@ const Header = () => {
   return (
     <header className="header">
       <h1>Jeff Stinson</h1>
-      <div className="header-links">
-        <button href="About">About Me</button>
-        <button href="Projects" onClick={() => handleClickScroll("projects")}>
+      <nav className="header-links">
+        <button onClick={() => handleClickScroll("about")} >About Me</button>
+        <button onClick={() => handleClickScroll("projects")}>
           Projects
         </button>
-        <button href="Skills">Skills</button>
-        <button href="Resume">Resume</button>
-        <button href="Contact">Contact</button>
-      </div>
+        <button onClick={() => handleClickScroll("skills")}>Skills</button>
+        <button onClick={() => handleClickScroll("resume")}>Resume</button>
+        <button onClick={() => handleClickScroll("contact")}>Contact</button>
+      </nav>
     </header>
   );
 };
