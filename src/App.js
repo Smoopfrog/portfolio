a6799d9d9a75180e44c3da36a782714e75a68ec3
Smoopfrog <jeffstinson@hotmail.com>
2023-04-26 00:23:47 -0700

    create about section and basic layout styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import About from "./Components/About";
 // import Body from "./Components/Body";
 import Header from "./Components/Header";
 // import Project from "./Components/Projects/Project";
@@ -8,6 +9,7 @@ const App = () => {
   return (
     <div className="App">
       <Header />
+      <About />
       <ProjectFeed />
     </div>
   );
---
+++ src/Components/About.jsx
@@ -0,0 +1,24 @@
+import "../Styles/About.css";
+
+const About = () => {
+  return (
+    <section className="about">
+      <div className="about-info">
+        <h1>Jeff Stinson</h1>
+        <p>
+          A full-stack developer, with a background in hospitality management,
+          keen to dive into the world of web development. Graduated from
+          Lighthouse Labs where I became proficient with front and backend
+          technologies.
+        </p>
+      </div>
+      <div>
+        <a href="https://www.linkedin.com/in/jeff-stinson94/">linkedin</a>
+        <a href="https://github.com/Smoopfrog">Github</a>
+        <a href="/">Email</a>
+      </div>
+    </section>
+  );
+};
+
+export default About;
