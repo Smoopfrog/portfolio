518630fca4eb24cc9b870491ccb9151ac2147d03
Jeffrey Stinson <jeffstinson@hotmail.com>
2023-04-27 21:02:19 -0700

    fix scroll when clicking on header link

--- src/Components/Projects/ProjectFeed.jsx
+++ src/Components/Projects/ProjectFeed.jsx
@@ -2,10 +2,9 @@ import Project from "./Project";
 import "../../Styles/ProjectFeed.css";
 
 const ProjectFeed = () => {
-
   return (
-    <section className="projects-container">
-      <h1 className="section-title" id="projects">Projects</h1>
+    <section className="projects-container" id="projects">
+      <h1 className="section-title">Projects</h1>
       <Project />
       <Project />
       <Project />
