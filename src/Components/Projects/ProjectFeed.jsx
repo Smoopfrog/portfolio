6b1f045df758672e12d3fb8c26bc83acd0a9ad9c
Smoopfrog <jeffstinson@hotmail.com>
2023-04-29 23:16:39 -0700

    add real data to projects

--- src/Components/Projects/ProjectFeed.jsx
+++ src/Components/Projects/ProjectFeed.jsx
@@ -5,9 +5,56 @@ import next from "../../Assets/next.png";
 import back from "../../Assets/back.png";
 
 const projects = [
-  { title: "Initiative" },
-  { title: "Betbook" },
-  { title: "Frame Fitness" },
+  {
+    title: "Initiative",
+    description:
+      "Create characters, find monsters using D&D 5e API, and track initiative and stats for Dungeons and Dragons encounters.",
+    tech: [
+      "HTML",
+      "CSS",
+      "Javascript",
+      "React",
+      "Redux Toolkit",
+      "Express",
+      "PSQL",
+      "Axios",
+      "Bcrypt",
+    ],
+    url: "https://initiative.herokuapp.com/",
+    github: "https://github.com/Smoopfrog/Initiative",
+  },
+  {
+    title: "Betbook",
+    description:
+      "Keep track of your bets and stats with this easy to use React Native mobile app",
+    tech: [
+      "React Native",
+      "React Navigation",
+      "Expo",
+      "Firebase",
+      "Redux Toolkit",
+      "Moment",
+    ],
+    url: null,
+    github: "https://github.com/Smoopfrog/bet-book",
+  },
+  {
+    title: "Frame Fitness",
+    description:
+      "Signup or login to choose from a library of 1300+ exercises categorized by chest, arms, legs, forearms, lower legs, back, shoulders, neck, and waist!",
+    tech: [
+      "HTML",
+      "React",
+      "SASS",
+      "Material UI",
+      "Express",
+      "Psql",
+      "Axios",
+      "Socket.io",
+    ],
+    url: null,
+    github: "https://github.com/Smoopfrog/frame-fitness ",
+  },
 ];
 
 const ProjectFeed = () => {
@@ -48,7 +95,7 @@ const ProjectFeed = () => {
                 }
               >
                 {index === currentIndex && (
-                  <Project title={project.title} key={index} />
+                  <Project project={project} key={index} />
                 )}
               </div>
             );
