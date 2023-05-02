b570a2786ecc9001dac587993a6061cb80e1ec68
Smoopfrog <jeffstinson@hotmail.com>
2023-05-01 22:00:47 -0700

    add tech stack to about section

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -5,12 +5,13 @@ const About = () => {
     <section className="about" id="about">
       <div className="about-info">
         <div>
-          <h1>Jeff </h1>
-          <h1>Stinson</h1>
+          <h1>Full-Stack Web Developer</h1>
+          {/* <h1>Stinson</h1> */}
         </div>
         <p>
-          A full-stack developer, with a background in hospitality management,
-          keen to dive into the world of web development.
+          Hello, I'm Jeff Stinson. A full-stack developer, with a background in
+          hospitality management, keen to dive into the world of web
+          development.
         </p>
       </div>
       <div className="about-links">
@@ -34,12 +35,29 @@ const About = () => {
           </svg>
           <div className="social-anchor-circle"></div>
         </a>
-        <a href="mailto: jeffstinson@hotmail.com" target="_blank" rel="noopener noreferrer">
+        <a
+          href="mailto: jeffstinson@hotmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <svg className="social-icon" viewBox="0 0 512 512">
             <path d="M101.3 141.6v228.9h0.3 308.4 0.8V141.6H101.3zM375.7 167.8l-119.7 91.5 -119.6-91.5H375.7zM127.6 194.1l64.1 49.1 -64.1 64.1V194.1zM127.8 344.2l84.9-84.9 43.2 33.1 43-32.9 84.7 84.7L127.8 344.2 127.8 344.2zM384.4 307.8l-64.4-64.4 64.4-49.3V307.8z"></path>
           </svg>
           <div className="social-anchor-circle"></div>
         </a>
+      </div>
+      <div className="about__tech-stack">
+        <div>Tech Stack</div>
+        <div>HTML</div>
+        <div>CSS</div>
+        <div>Javascript</div>
+        <div>React</div>
+        <div>React Native</div>
+        <div>SASS</div>
+        <div>Material UI</div>
+        <div>NodeJS</div>
+        <div>Firebase</div>
+        <div>PSQL</div>
       </div>
     </section>
   );
