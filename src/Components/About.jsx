fdd44f4a06cd6b46167ce6a3c9373cd2a4c5918c
Smoopfrog <jeffstinson@hotmail.com>
2023-05-04 17:30:33 -0700

    add image to about section

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,12 +1,13 @@
 import "../Styles/About.css";
+import codingImg from "../Assets/coding.jpg"
 
 const About = () => {
   return (
     <section id="about" className="about">
-      <div>
-        <img src="" alt="" />
+      <div className="about-image">
+        <img src={codingImg} alt="coding" title="https://www.freepik.com/free-vector/hand-drawn-web-developers_12063795.htm#query=programmer%20illustration&position=9&from_view=keyword&track=ais" />
       </div>
-      <div>
+      <div className="about-info">
         <h1>About me</h1>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident
