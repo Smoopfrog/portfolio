ec5c3eeb3ed2caf107cc7775cc625e9ffcb9aea9
Smoopfrog <jeffstinson@hotmail.com>
2023-05-04 18:25:58 -0700

    add rotating hire me icon in about image

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,19 +1,38 @@
 import "../Styles/About.css";
-import codingImg from "../Assets/coding.jpg"
+import codingImg from "../Assets/coding.jpg";
+import frontEnd from "../Assets/front-end.svg";
 
 const About = () => {
   return (
     <section id="about" className="about">
       <div className="about-image">
-        <img src={codingImg} alt="coding" title="https://www.freepik.com/free-vector/hand-drawn-web-developers_12063795.htm#query=programmer%20illustration&position=9&from_view=keyword&track=ais" />
+        <img
+          className="coding-img"
+          src={codingImg}
+          alt="coding"
+          title="https://www.freepik.com/free-vector/hand-drawn-web-developers_12063795.htm#query=programmer%20illustration&position=9&from_view=keyword&track=ais"
+        />
+        <div className="about-hire">
+          <img className="background-img" src={frontEnd} alt="" /> Hire me
+          please
+          <br /> Im desperate
+        </div>
       </div>
       <div className="about-info">
         <h1>About me</h1>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident
-          distinctio iure nam dolore architecto natus eius cum asperiores sequi
-          rerum quas corrupti id, tempore inventore nostrum ducimus eum animi
-          harum?
+          A full-stack developer, with a background in hospitality management,
+          keen to dive into the world of web development. Graduated from
+          Lighthouse Labs where I became proficient with front and backend
+          technologies such as HTML, CSS, Sass, Javascript, React, and Express,
+          along with many other skills. Seeking opportunities, virtually or in
+          Vancouver.
+          <br />
+          <br />
+          Let's connect!
+          <br />
+          <br />
+          P.S. I bake cookies for my coworkers.
         </p>
       </div>
     </section>
